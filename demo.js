1062ea21337f037c0e61fccdb62e1d20a6505a01
SzortykaZSK <119489598+SzortykaZSK@users.noreply.github.com>
2023-09-20 09:05:44 +0200

    Delete demo.js

--- demo.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  readline.question('Podaj wartosc', (value) => {
-    
-    readline.close();
-  });
