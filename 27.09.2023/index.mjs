8a8c518b846765f5727c56d63c77a500f43c0228
SzortykaZSK <119489598+SzortykaZSK@users.noreply.github.com>
2023-10-02 09:33:11 +0200

    Add files via upload
    dodanie zadania z routingiem

---
+++ 27.09.2023/index.mjs
@@ -0,0 +1,36 @@
+import * as http from 'http';
+
+import { readFile } from 'fs/promises';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+  const url = req.url;
+
+  if (url === '/') {
+    res.statusCode = 200;
+
+    const html = await readFile('./html/home.html');
+
+    res.setHeader('content-type', 'text/html');
+
+    res.write(html);
+
+    res.end();
+  } else if (url === '/contact') {
+    res.statusCode = 200;
+
+    const html = await readFile('./html/home.html');
+
+    res.setHeader('content-type', 'text/html');
+
+    res.write(html);
+
+    res.end();
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
