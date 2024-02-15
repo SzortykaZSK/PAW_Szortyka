d317c6685891b8b9d0738d084ba385c0d6c32b2c
Mateusz Szortyka <119489598+SzortykaZSK@users.noreply.github.com>
2024-02-15 20:33:39 +0100

    Delete Zadanie React directory

--- Zadanie React/kod/src/components/Footer/index.tsx
+++
@@ -1,5 +0,0 @@
-import './footer.scss'
-
-export default function Footer() {
-  return <footer id="app-footer">Copyright 2024 &copy; Jakub Szrama | 4D</footer>
-}
--- Zadanie React/kod/src/components/Paragraph/index.tsx
+++
@@ -1,9 +0,0 @@
-import './paragraph.scss'
-
-interface ParagraphProps {
-  content: string
-}
-
-export default function Paragraph({content}: ParagraphProps) {
-  return <p>{content}</p>
-}
